4b4405a1b20c91bc215e23929f9e6b5709e1d634
Sinéad McIntyre <shins.mcintyre@gmail.com>
2026-01-08 12:01:59 +0000

    feat: import API to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+// import dotenv from "dotenv"
 import "./App.css";
 
 // ! DO NOT PUT ALL YOUR CODE IN APP.JSX - USE COMPONENTS
@@ -7,10 +8,21 @@ function App() {
   // STATE
   // - variable to store API image data
   // - variable to store current image
+  const [photos, setPhotos] = useState([]);
 
   // EFFECTS
   // - fetch data from the API
   // - once it's fetched, put it in state
+  useEffect(() => {
+    async function getPhotos() {
+      // TODO: use env to hide the access key - check API workshop
+      const response = await fetch(import.meta.env.VITE_PHOTO_API_URL);
+      const data = await response.json();
+      setPhotos(data);
+    }
+
+    getPhotos();
+  }, []);
 
   // FUNCTIONS (EVENT HANDLERS)
   // - when a user clicks an image
@@ -20,7 +32,20 @@ function App() {
     <>
       <h1>Gallery</h1>
 
-      <div>Thumbnail container, showing all the images using .map()</div>
+      {/* TODO: CHANGE THE STYLING OF THE GALLERY */}
+      <div style={{ display: "flex", gap: "10px", flexWrap: "wrap" }}>
+        <p>Thumbnail container, showing all the images using .map()</p>
+        {photos.map((photo) => (
+          <img
+            key={photo.id}
+            src={photo.urls.small}
+            alt={photo.alt_description}
+            width="200"
+          />
+        ))}
+      </div>
+      {/* THIS WILL NEED TO CHANGE/BE ADDED INTO COMPONENT? */}
+      {photos && <img src={photos} alt="Photo" width="400" />}
 
       <div>
         One big image, or a modal that is only sometimes there (conditional
